76fc7d39534cd68a7b2ba2e012849f5f9b70ae4c
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-05-21 14:37:40 -0500

    while i < 4 :

---
+++ InEasyStepsPy/Chapter03_while.py
@@ -0,0 +1,16 @@
+# chapter 3 while loop
+i = 1
+while i < 4 :
+	print('\nOuter loop interation:', i)
+	i += 1
+	j = 1
+	while j < 4 :
+		print('\tInner loop interations:', j)
+		j += 1
+
+
+
+
+
+
+
