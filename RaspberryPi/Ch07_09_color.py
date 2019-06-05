48cb5a07bb6a6ebdf2bcdb78d19394312816e1d3
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-06-04 21:46:27 -0500

    colorchooser

---
+++ RaspberryPi/Ch07_09_color.py
@@ -0,0 +1,15 @@
+# dialogs
+from tkinter import *
+import tkinter.colorchooser as cc
+
+class App:
+	def __init__(self, master):
+		b = Button(master, text='Color..', command=self.ask_color).pack()
+		
+	def ask_color(self):
+		(rgb, hx) = cc.askcolor()
+		print("rgb=", str(rgb) + " hx=" + hx)
+		
+root = Tk()
+app = App(root)
+root.mainloop()
