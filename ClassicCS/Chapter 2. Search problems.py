7d1bfee38e5a0c333b6163df3e253a1396d1882a
PyRPy <likedatascience@gmail.com>
2019-11-15 20:58:18 -0600

    search agorithms

---
+++ ClassicCS/Chapter 2. Search problems.py
@@ -0,0 +1,129 @@
+# Chapter 2. Search problems
+
+# 2.1. DNA search
+# 2.1.1. Storing DNA
+from enum import IntEnum
+from typing import Tuple, List
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # type alias for codons
+Gene = List[Codon] # type alias for genes
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s): # don't run off end!
+            return gene
+        # initialize codon out of three nucleotides
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon) # add codon to gene
+    return gene
+
+my_gene: Gene = string_to_gene(gene_str)
+print(my_gene)
+
+# 2.1.2. Linear search
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+print(linear_contains(my_gene, acg)) # True
+print(linear_contains(my_gene, gat)) # False
+
+# 2.1.3 Binary search
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high: # while there is still a search space
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+my_sorted_gene: Gene = sorted(my_gene)
+print(binary_contains(my_sorted_gene, acg)) # True
+print(binary_contains(my_sorted_gene, gat)) # False
+
+# 2.1.4 A generic example
+
+from __future__ import annotations
+from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional
+from typing_extensions import Protocol
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    return False
+C = TypeVar("C", bound="Comparable")
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+    def __lt__(self: C, other: C) -> bool:
+            ...
+    def __gt__(self: C, other: C) -> bool:
+        return (not self < other) and self != other
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+
+def binary_contains(sequence: Sequence[C], key: C) -> bool:
+    low: int = 0
+    high: int = len(sequence) - 1
+    while low <= high: # while there is still a search space
+        mid: int = (low + high) // 2
+        if sequence[mid] < key:
+            low = mid + 1
+        elif sequence[mid] > key:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5)) # True
+print(binary_contains(["a", "d", "e", "f", "z"], "f")) # True
+print(binary_contains(["john", "mark", "ronald", "sarah"], "sheila"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
