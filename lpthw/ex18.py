5b0c055752dd9d5d2f47d33f2158eabcc8bcc87e
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-21 22:36:35 -0600

    names, variables, code, functions

---
+++ lpthw/ex18.py
@@ -0,0 +1,21 @@
+# this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# this just takes one argument
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# this one takes no arguments
+def print_none():
+    print("i got nothing'.")
+
+print_two("zed", "shaw")
+print_two_again("me", "you")
+print_one("me")
+print_none()
