5ebb2dfc127caae46f0847ca7f23bc8539b0f418
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-21 20:51:25 -0600

    reading files

---
+++ lpthw/ex15.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"here is your file {filename}:")
+print(txt.read())
+
+print("type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+txt_again.close()
+txt.close()
