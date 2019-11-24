e2530287a6a65776e2e4ae54dca9a1629127b0da
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-23 20:16:57 -0600

    branches and functions

---
+++ lpthw/ex35.py
@@ -0,0 +1,77 @@
+from sys import exit
+
+def gold_room():
+    print("this room is full of gold, how much do you take?")
+
+    choice = input("> ")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number.")
+
+    if how_much < 50:
+        print("nice, you are not greedy, you win!")
+        exit(0)
+    else:
+        dead("you greed bastard!")
+
+
+def bear_room():
+    print("there is a bear here.")
+    print("the bear has a bunch of honey.")
+    print("the fat bear is in front of another door.")
+    print("how are you going to move the bear?")
+    bear_moved = False
+
+    while True:
+        choice = input("> ")
+
+        if choice == "take money":
+            dead("the bear looks at you then slaps your face off.")
+        elif choice == "taunt bear" and not bear_moved:
+            print("the bear has moved from the door.")
+            print("you can go through it now.")
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("the bear get pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("i got no idea what that means.")
+
+
+def cthulhu_room():
+    print("here you see the great evil Cthulhu")
+    print("he, it, whatever stares at you and you go insane.")
+    print("do you flee for your life oreat your head?")
+
+    choice = input("> ")
+
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("well that was tasty!")
+    else:
+        cthulhu_room()
+
+
+def dead(why):
+    print(why, "good job!")
+    exit(0)
+
+def start():
+    print("you are in a dark room.")
+    print("there is a door to your right and left.")
+    print("which one do you take?")
+
+    choice = input("> ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("you stumble around the room until you starve.")
+
+
+start()
