387320f510039d316d3543d09330fd4b9890626c
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-26 21:09:21 -0600

    add connection to index.html - server not working

--- lpthw/app.py
+++ lpthw/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    greeting = "world"
-    return 'Hello, {greeting}!'
+def index():
+    greeting = "Hello world"
+    return render_template("index.html", greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
