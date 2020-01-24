af1ba8bb8d776a967c5a37e002d2c9f5098d7927
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-01-23 20:23:02 -0600

    first recursive example

---
+++ leetcode/generate_parenthesis.py
@@ -0,0 +1,27 @@
+# generate parenthesis
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        type n: int
+        rtype: list[str]
+        """
+
+        if n == 0:
+            return []
+
+        result = []
+        self.helper(n, n, '', result)
+        return result
+
+    def helper(self, l, r, item, result):
+        if r < l:
+            return
+        if l == 0 and r == 0:
+            result.append(item)
+        if l > 0:
+            self.helper(l-1, r, item + '(', result)
+        if r > 0:
+            self.helper(l, r-1, item + ')', result)
+
+sol = Solution()
+print(sol.generateParenthesis(3))
