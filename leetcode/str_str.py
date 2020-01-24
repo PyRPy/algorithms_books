42c5ce2eb0431c2eaa10facf9890b979662a6b39
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-01-24 15:21:41 -0600

    strings match

---
+++ leetcode/str_str.py
@@ -0,0 +1,19 @@
+# str str
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        type haystack:str
+        type needle: str
+        rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+sol = Solution()
+needle = "ll"
+haystack = "parallel"
+
+print(haystack)
+print(sol.strStr(haystack, needle))
