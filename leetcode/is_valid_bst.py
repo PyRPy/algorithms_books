8f3c07073826b020889f7da9fa77014e6b3d20ff
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-03 22:58:30 -0500

    is a true binary search tree ? need a example

---
+++ leetcode/is_valid_bst.py
@@ -0,0 +1,22 @@
+# is a binary search tree or not
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None 
+        self.right = None 
+
+class Solution:
+    def isValidBST(self, root):
+        """
+        type root: TreeNode 
+        rtype: bool
+        """
+
+        return self.valid(root, float("-inf"), float("inf"))
+    def valid(self, root, min, max):
+        if not root:
+            return True 
+        if root.val >= max or root.val <= min:
+            return False 
+
+        return self.valid(root.left, min, root.val) and self.valid(root.right, root.val, max)
