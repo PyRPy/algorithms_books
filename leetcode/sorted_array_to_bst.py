e5828199febf21dba0fa18d8d711b1dbcf79556d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-03 22:57:53 -0500

    sorted array converted to bst - need an example

---
+++ leetcode/sorted_array_to_bst.py
@@ -0,0 +1,25 @@
+# convert sorted array to binary search tree
+# define a binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x 
+        self.left = None 
+        self.right = None 
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        type nums: list[int]
+        rtype: TreeNode
+        """
+        def to_bst(nums, start, end):
+            if start > end:
+                return None
+            mid = (start + end) // 2
+            node = TreeNode(nums[mid])
+            node.left = to_bst(nums, start, mid-1) 
+            node.right = to_bst(nums, mid + 1, end)
+            return node 
+
+        return to_bst(nums, 0, len(nums) - 1)
+        
