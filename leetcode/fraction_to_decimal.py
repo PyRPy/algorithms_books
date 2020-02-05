c49472bc53fe2fdacf55154995457f6fc1422c4e
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-04 23:20:29 -0500

    difficult one, how to show 0.33333

---
+++ leetcode/fraction_to_decimal.py
@@ -0,0 +1,42 @@
+# fraction to recurring decimal
+class Solution(object):
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        type numerator: int 
+        type denominator: int
+        rtype: str
+        """
+        negativeFlag = numerator * denominator < 0
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+
+        numList = []
+        cnt = 0
+        loopDict = dict()
+        loopStr = None 
+        while True:
+            numList.append(str(numerator // denominator))
+            cnt += 1
+            numerator = 10 * (numerator % denominator)
+            if numerator == 0:
+                break 
+            loc = loopDict.get(numerator)
+            if loc:
+                loopStr = "".join(numList[loc:cnt]) 
+                break
+            loopDict[numerator] = cnt
+        ans = numList[0]
+        if len(numList) > 1:
+            ans += "."
+        if loopStr:
+            ans += "".join(numList[1: len(numList) - len(loopStr)]) + "(" + loopStr + ")"
+        else:
+            ans += "".join(numList[1:])
+        if negativeFlag:
+            ans = "-" + ans 
+        return ans
+sol = Solution()
+numerator = 1
+denominator = 3
+print(numerator, denominator)
+print(sol.fractionToDecimal(numerator, denominator))
