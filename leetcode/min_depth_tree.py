82f79ea0438bc8ef1471208be60dd7190d127792
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-05 22:46:34 -0500

    minimum depth of a tree

---
+++ leetcode/min_depth_tree.py
@@ -0,0 +1,20 @@
+# minimum depth of a tree
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x 
+        self.left = None 
+        self.right = None 
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        type root: TreeNode
+        rtype: int
+        """
+
+        if root is None:
+            return 0
+        if root.left and root.right:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
