4b3e912d53058da12535fea1bbbc081e82cb9294
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-10 22:25:25 -0500

    valid palindrome - symmetrical or not

---
+++ leetcode/valid_palindrome.py
@@ -0,0 +1,29 @@
+# valid palindrome
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        type s: str
+        rtype: bool
+        """
+        i, j = 0, (len(s)-1)
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True 
+
+s = "race a car"
+print(s)
+sol = Solution()
+print(sol.isPalindrome(s))
+
+s = "mom"
+print(s)
+sol = Solution()
+print(sol.isPalindrome(s))
