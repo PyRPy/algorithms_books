e200ae31051c336911cf1d23777eb175722619ca
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-12 21:53:48 -0500

    inverse or reverse thinking ?

---
+++ leetcode/linked_list_cycle.py
@@ -0,0 +1,20 @@
+# linked list cycle or not
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x 
+        self.next = None 
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        type head: ListNode
+        rtype: bool
+        """
+        fast, slow = head, head 
+
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next 
+            if fast == slow:
+                return True 
+        return False
+        
