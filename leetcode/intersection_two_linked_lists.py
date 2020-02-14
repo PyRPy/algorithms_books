6157447f0dd41fa50bed616512085efd2b501f88
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-14 00:21:18 -0500

    intersection - two pointers

---
+++ leetcode/intersection_two_linked_lists.py
@@ -0,0 +1,27 @@
+# intersection of two linked lists
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None 
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        type head1, head2: ListNode
+        rtype: ListNode
+        """
+        p1 = headA
+        p2 = headB 
+
+        while p1 != p2:
+            if not p1:
+                p1 = headB 
+            else:
+                p1 = p1.next 
+
+            if not p2:
+                p2 = headA 
+            else:
+                p2 = p2.next 
+        return p2 
+        
