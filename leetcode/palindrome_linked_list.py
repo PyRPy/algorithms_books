1fdb2c1d233778ec1a6f52736ecf9f24a1fcb1c8
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-25 21:03:59 -0600

    stack, try something intuitive...

---
+++ leetcode/palindrome_linked_list.py
@@ -0,0 +1,27 @@
+# palindrome linked list
+# definition for singly linked list
+class ListNode:
+    def __int__(self, x):
+        self.val = x 
+        self.next = None 
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = slow = ListNode(0)
+        fast = slow = head 
+        stack = []
+
+        while fast and fast.next:
+            stack.append(slow.val)
+            slow = slow.next 
+            fast = fast.next.next 
+
+        if fast:
+            slow = slow.next 
+        while slow:
+            top = stack.pop()
+
+            if top != slow.val:
+                return False
+            slow = slow.next 
+        return True 
