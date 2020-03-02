2586283ef9747f9c67f8a608f08c70ed4a1688ff
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-03-01 22:27:37 -0500

    start from small programs and hone skills !
    https://mcsp.wartburg.edu/zelle/python/

---
+++ BookZelle/01_chaos.py
@@ -0,0 +1,16 @@
+# chaos.py
+# excercise 7
+def main():
+    """
+    a simple program illustrating chaotic behavior
+    """
+
+    print("this program illustrates a chaotic function")
+    x = eval(input("enter first number between 0 and 1: "))
+    y = eval(input("enter second0.2 number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        y = 3.9 * y * (1 - y)
+        print(x, " ", y)
+
+main()
