0c9293f9ec305f44b4baa2706bf9bee31a951edb
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-03-01 22:29:20 -0500

    correct a typo in print statement

--- BookZelle/01_chaos.py
+++ BookZelle/01_chaos.py
@@ -7,7 +7,7 @@ def main():
 
     print("this program illustrates a chaotic function")
     x = eval(input("enter first number between 0 and 1: "))
-    y = eval(input("enter second0.2 number between 0 and 1: "))
+    y = eval(input("enter second number between 0 and 1: "))
     for i in range(10):
         x = 3.9 * x * (1 - x)
         y = 3.9 * y * (1 - y)
