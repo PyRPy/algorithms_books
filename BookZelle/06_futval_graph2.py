70bd0712a34052ab4b1481293e089f1f00238260
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-04-01 22:31:43 -0500

    before using function or functions...
    https://mcsp.wartburg.edu/zelle/python/

---
+++ BookZelle/06_futval_graph2.py
@@ -0,0 +1,40 @@
+# 06_futval_graph2.py
+
+from graphics import *
+def main():
+    # intro
+    print("plot the growth of investment")
+
+    # get principal and inerest rate
+    principal = float(input("enter the initial principal: "))
+    apr = float(input("enter the annualized interest rate: "))
+
+    # create graph
+    win = GraphWin("Investment Growth Chart", 320, 240)
+    win.setBackground("white")
+    win.setCoords(-1.75, -200, 11.5, 10400)
+    Text(Point(-1,0), ' 0.0K').draw(win)
+    Text(Point(-1,2500), ' 2.5K').draw(win)
+    Text(Point(-1,5000), ' 5.0K').draw(win)
+    Text(Point(-1,7500), ' 7.5K').draw(win)
+    Text(Point(-1,10000), ' 10.0K').draw(win)
+
+    # draw bar for initial principal
+    bar = Rectangle(Point(0,0), Point(1, principal))
+    bar.setFill("green")
+    bar.setWidth(2)
+    bar.draw(win)
+
+    # draw a bar for each following year
+
+    for year in range(1, 11):
+        principal = principal * (1 + apr)
+        bar = Rectangle(Point(year, 0), Point(year + 1, principal))
+        bar.setFill('green')
+        bar.setWidth(2)
+        bar.draw(win)
+
+    input("press <enter> to quit.")
+    win.close()
+
+main()
