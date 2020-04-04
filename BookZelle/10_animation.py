e23ca59bd3b91e784d7ccdbb55b139e6788d43be
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-04-04 16:33:32 -0500

    canon ball - class + interface + OOP intro

---
+++ BookZelle/10_animation.py
@@ -0,0 +1,123 @@
+# 10_animation.py
+
+from math import sqrt, sin, cos, radians, degrees 
+from graphics import * 
+from projectile import Projectile 
+from button import Button 
+
+class ShortTracker:
+    def __init__(self, win, angle, velocity, height):
+        """Win is the GraphWin to display the shot angle, velocity,
+        and height are initial projectile parameters.
+        """ 
+        self.proj = Projectile(angle, velocity, height) 
+        self.marker = Circle(Point(0, height), 3) 
+        self.marker.setFill("red")
+        self.marker.setOutline("red")
+        self.marker.draw(win) 
+
+    def update(self, dt):
+        """ Move the shot dt seconds farther along its flight """ 
+        # update the projectile 
+        self.proj.update(dt) 
+
+        # move the circle to the new projectile location 
+        center = self.marker.getCenter() 
+        dx = self.proj.getX() - center.getX() 
+        dy = self.proj.getY() - center.getY() 
+        self.marker.move(dx, dy) 
+
+    def getX(self):
+        return self.proj.getX() 
+
+    def getY(self):
+        return self.proj.getY() 
+
+    def undraw(self):
+        self.marker.undraw() 
+
+class InputDialog:
+    """ A custom window for getting simulation values(angle, vel, and height) from the 
+        user. """ 
+
+    def __init__(self, angle, vel, height):
+        self.win = win = GraphWin("Initial values", 200, 300)
+        win.setCoords(0, 4.5, 4, 0.5) 
+
+        Text(Point(1,1), "Angle").draw(win) 
+        self.angle = Entry(Point(3,1), 5).draw(win) 
+        self.angle.setText(str(angle)) 
+
+        Text(Point(1,2), "velocity").draw(win) 
+        self.vel = Entry(Point(3,2), 5).draw(win) 
+        self.vel.setText(str(vel)) 
+
+        Text(Point(1,3), "Height").draw(win) 
+        self.height = Entry(Point(3,3), 5).draw(win) 
+        self.height.setText(str(height)) 
+
+        self.fire = Button(win, Point(1,4), 1.25, 0.5, "Fire!") 
+        self.fire.activate() 
+
+        self.quit = Button(win, Point(3,4), 1.25, 0.5, "Quit") 
+        self.quit.activate() 
+
+    def interact(self):
+        """Wait for user to click Quit or Fire button 
+        Returns a string indicating which button was clicked 
+        """ 
+
+        while True:
+            pt = self.win.getMouse() 
+            if self.quit.clicked(pt):
+                return "Quit" 
+            if self.fire.clicked(pt):
+                return "Fire!" 
+
+    def getValues(self):
+        """ return input values """ 
+        a = float(self.angle.getText()) 
+        v = float(self.vel.getText()) 
+        h = float(self.height.getText()) 
+        return a,v,h 
+
+    def close(self):
+        """ close the input window """ 
+        self.win.close() 
+
+
+def main():
+    # create animation window 
+    win = GraphWin("projectile animation", 640, 480, autoflush = False) 
+    win.setCoords(-10, -10, 210, 155) 
+
+    # draw baseline 
+    Line(Point(-10, 0), Point(210, 0)).draw(win) 
+
+    # draw labeled ticks every 50 meters 
+    for x in range(0, 210, 50):
+        Text(Point(x, -5), str(x)).draw(win) 
+        Line(Point(x,0), Point(x,2)).draw(win) 
+
+        # event loop, each time through fires a single shot 
+        angle, vel, height = 45.0, 40.0, 2.0 
+        while True:
+            # interact with the user 
+            inputwin = InputDialog(angle, vel, height) 
+            choice = inputwin.interact() 
+            inputwin.close() 
+
+            if choice == "Quit": 
+                break 
+
+            # create a shot and track until it hits ground or leaves window 
+            angle, vel, height = inputwin.getValues() 
+            shot = ShortTracker(win, angle, vel, height) 
+            while 0<= shot.getY() and -10 < shot.getX() <= 210:
+                shot.update(1/50) # this 'update()' comes from class method
+                update(50) # this function comes from 'graphics.py'
+
+        win.close() 
+
+if __name__ == "__main__":
+    main() 
