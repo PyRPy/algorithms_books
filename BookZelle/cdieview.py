1b5e70a7cfe41a046457a603f41db16b6bf4f53d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-04-09 10:56:00 -0500

    12 Dice Poker - GUI

---
+++ BookZelle/cdieview.py
@@ -0,0 +1,15 @@
+# cdieview.py
+# implementation of a DieView with changeable forground color
+# illustrates inheritance 
+
+from dieview import DieView 
+
+class ColorDieView(DieView):
+
+    def setValue(self, value):
+        self.value = value          # remember this value 
+        DieView.setValue(self, value) # call setValue from parent class 
+
+    def setColor(self, color):
+        self.foreground = color 
+        self.setValue(self.value) 
