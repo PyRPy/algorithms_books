132e3b1a3cc88c2d9eb58d9e6273e0d99dd9d489
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-04 12:04:44 -0500

    concept of 'node' prior to linked-list

---
+++ pythonds/4_Node_class.py
@@ -0,0 +1,20 @@
+# 4_Node_class.py
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata 
+        self.next = None 
+
+    def getData(self):
+        return self.data 
+
+    def getNext(self):
+        return self.next 
+
+    def setData(self, newdata):
+        self.data = newdata 
+
+    def setNext(self, newnext):
+        self.next = newnext 
+
+temp = Node(93) 
+print(temp.getData())
