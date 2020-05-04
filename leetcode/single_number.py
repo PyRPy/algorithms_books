78a7608ffc4bef3df2fa9835a5721e463a5ff0c6
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-03 23:08:36 -0500

    add another methods - 'operator' + 'functools'

--- leetcode/single_number.py
+++ leetcode/single_number.py
@@ -1,4 +1,7 @@
 # find the single number in the array
+import operator
+from functools import reduce 
+
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -11,7 +14,14 @@ class Solution(object):
 
         return ans 
 
+
+    def singleNumber2(self, A):
+        return reduce(operator.xor, A) 
+
 nums = [3, 2, 4, 2, 4, 0]
+nums2 = [1, 1, 3, 0, 2, 4, 2, 4]
 sol = Solution()
 print(nums)
 print(sol.singleNumber(nums))
+print("second method: ", sol.singleNumber2(nums))
+print("second method: ", sol.singleNumber2(nums2))
