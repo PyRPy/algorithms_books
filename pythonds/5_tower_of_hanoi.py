0ada60613cab4a310fc7ce2ef0bd2b20f88fd4e5
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-05 09:01:29 -0500

    how to map three conditions of  a recursive func

---
+++ pythonds/5_tower_of_hanoi.py
@@ -0,0 +1,11 @@
+# 5_tower_of_hanoi.py
+def moveTower(height, fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1, fromPole, withPole, toPole) 
+        moveDisk(fromPole, toPole) 
+        moveTower(height-1, withPole, toPole, fromPole) 
+
+def moveDisk(fp, tp):
+    print("moving disk from: ", fp, " to", tp) 
+
+moveTower(3, "A", "B", "C")
