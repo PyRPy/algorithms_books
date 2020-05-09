94e5d48d339c964d6a0a400f32ad82bd192a26b7
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-09 12:25:09 -0500

    heap - easy
    https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/kth-largest-element-in-a-stream.py

---
+++ leetcodeDS/kth_largest_number_stream.py
@@ -0,0 +1,28 @@
+# kth_largest_number_stream.py
+import heapq 
+
+class KthLargest(object):
+    def __init__(self, k, nums):
+        """ 
+        :type k: init 
+        :type nums: List[int] 
+        """ 
+        self.__k = k  
+        self.__min_heap = [] 
+        for n in nums: 
+            self.add(n) 
+
+    def add(self, val):
+        """ 
+        :type val: int 
+        :rtype: int 
+        """ 
+        heapq.heappush(self.__min_heap, val) 
+        if len(self.__min_heap) > self.__k: 
+            heapq.heappop(self.__min_heap) 
+        return self.__min_heap[0] 
+
+k = 3 
+arr = [4, 5, 8, 2] 
+kthLargest = KthLargest(k, arr) 
+print(kthLargest.add(3))
