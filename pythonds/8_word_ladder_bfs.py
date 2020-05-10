cd104e3b50badffc3b550afbe8a2fd04d698e576
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-10 13:27:04 -0500

    use graph to do word ladder game - not complete

---
+++ pythonds/8_word_ladder_bfs.py
@@ -0,0 +1,58 @@
+# 8_word_ladder.py
+from pythonds.graphs import Graph, Vertex 
+from pythonds.basic import Queue 
+
+def buildGraph(wordfile):
+    d = {} 
+    g = Graph() 
+    wfile = open(wordfile, 'r')
+
+    # bucket of words thta differ by one letter 
+    for line in wfile:
+        word = line[:-1] 
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i+1:] 
+            if bucket in d:
+                d[bucket].append(word) 
+            else:
+                d[bucket] = [word] 
+
+    # add vertices and edges for words in the same bucket 
+    for bucket in d.keys():
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.addEdge(word1, word2) 
+    return g 
+
+def bfs(g, start):
+    start.setDistance(0) 
+    start.setPred(None) 
+    vertQueue = Queue() 
+    vertQueue.enqueue(start) 
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue() 
+        for nbr in currentVert.getConnections():
+            if nbr.getColor() == 'whilte':
+                nbr.setColor('gray')
+                nbr.setDistance(currentVert.getDistance() + 1) 
+                nbr.setPred(currentVert) 
+                vertQueue.enqueue(nbr) 
+        currentVert.setColor('black')
+
+def traverse(y):
+    x = y 
+    while(x.getPred()):
+        print(x.getId()) 
+        x = x.getPred() 
+    print(x.getID()) 
+
+
+wordfile = 'wordfile.txt'
+wg = buildGraph(wordfile)
+print(wg.numVertices)
+
+start = wg['FOOL']
+bfs(wg, start)
+
+# traverse(wg.getVertex('SAGE'))
