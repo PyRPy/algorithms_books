ab0e75ae111d56bb939796db9df1858867cc51bc
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-12 15:02:48 -0500

    add two more methods in different takes

--- pythonds/8_graph_tutorial_pyorg.py
+++ pythonds/8_graph_tutorial_pyorg.py
@@ -1,4 +1,5 @@
 # 8_graph_tutorial_pyorg.py 
+from collections import deque
 graph = {'A': ['B', 'C'],
          'B': ['C', 'D'],
          'C': ['D'],
@@ -20,9 +21,54 @@ def find_path(graph, start, end, path=[]):
                 return newpath 
     return None 
 
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start] 
+    if start == end:
+        return [path] 
+    if start not in graph:
+        return [] 
+    paths = [] 
+
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath) 
+    return paths 
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start] 
+    if start == end:
+        return path 
+    if start not in graph:
+        return None 
+    shortest = None 
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path) 
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath 
+    return shortest 
+
+def find_shortest_path2(graph, start, end):
+    dist = {start:[start]} 
+    q = deque(start) 
+    while len(q):
+        at = q.popleft() 
+        for next in graph[at]:
+            if next not in dist:
+                dist[next] = [dist[at], next] 
+                q.append(next) 
+    return dist.get(end) 
+
+
+
 print(find_path(graph, 'A', 'E'))
 print(find_path(graph, 'E', 'D'))
-
+print(find_all_paths(graph, 'A', 'D'))
+print(find_shortest_path(graph, 'A', 'D'))
+print(find_shortest_path2(graph, 'A', 'D')) # output format needs attention
 """
 reference and code sources:
 https://www.python.org/doc/essays/graphs/
