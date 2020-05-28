04680343a74ca03a2e6939eef0d09e048c89941c
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-05-28 11:44:11 -0500

    review on merge sort

---
+++ CLRS/gfg_merge_sort.py
@@ -0,0 +1,44 @@
+# gfg_merge_sort.py
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2 
+        L = arr[:mid]
+        R = arr[mid:] 
+
+        mergeSort(L) 
+        mergeSort(R) 
+
+        i = j = k = 0 
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i] 
+                i += 1 
+            else:
+                arr[k] = R[j] 
+                j += 1 
+            k += 1 
+
+        while i < len(L):
+            arr[k] = L[i] 
+            i += 1 
+            k += 1 
+        
+        while j < len(R): 
+            arr[k] = R[j] 
+            j += 1 
+            k += 1 
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end = " ")
+    print() 
+
+# test 
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7] 
+    printList(arr) 
+    mergeSort(arr) 
+    printList(arr) 
+    print(arr) # subtle difference
+                
