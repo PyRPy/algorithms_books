06383ea46797f6243576f89d1c0a94c732ba249d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-08-19 20:47:59 -0500

    add bubble sort - with early exit

--- Others/gfg_bubble_sort.py
+++ Others/gfg_bubble_sort.py
@@ -6,8 +6,26 @@ def bubbleSort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
 
+
+# have a swap flag
+def bubbleSort2(arr):
+    n = len(arr) 
+    for i in range(n):
+        swapped = False 
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                swapped = True
+        if swapped == False:
+            break 
+
 # test 
 arr = [64, 34, 25, 12, 22, 11, 90] 
+arr2 = [64, 34, 25, 12, 22, 11, 90] 
+
 bubbleSort(arr) 
 print('sorted array is: ', arr)
+
+bubbleSort2(arr2)
+print('second method: ', arr2)
 # https://www.geeksforgeeks.org/bubble-sort/
