032d909363f35c765092b4882bb299a7cbdccf36
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-08-22 21:32:13 -0500

    letter's concatenation...

---
+++ algoPy/1_ruler.py
@@ -0,0 +1,10 @@
+# 1_ruler.py
+ruler1 = '1'
+ruler2 = ruler1 + ' 2 ' + ruler1
+ruler3 = ruler2 + ' 3 ' + ruler2
+ruler4 = ruler3 + ' 4 ' + ruler3
+
+print(ruler1)
+print(ruler2)
+print(ruler3)
+print(ruler4)
