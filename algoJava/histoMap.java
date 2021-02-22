517357ff31737ef4630bc487339a3c2bad29f092
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-21 21:03:00 -0500

    map vs dict : java vs python

---
+++ algoJava/histoMap.java
@@ -0,0 +1,45 @@
+package hello;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.TreeMap;
+
+public class histoMap {
+
+	public static void main(String[] args) {
+		Scanner data = null;
+		TreeMap<String, Integer> count;
+		
+		Integer idx;
+		String word;
+		Integer wordCount;
+		
+		try {
+			data = new Scanner(new File("alice30.txt"));
+		}
+		catch (IOException e) {
+			System.out.println("sorry, unable to open your data");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		count = new TreeMap<String, Integer>();
+		
+		while (data.hasNext()) {
+			word = data.next().toLowerCase();
+			wordCount = count.get(word);
+			if (wordCount == null) {
+				wordCount = 0;
+			}
+			count.put(word, ++wordCount);
+		}
+		
+		for (String i : count.keySet() ) {
+			System.out.printf("%-20s occured %5d times\n", i, count.get(i));
+			
+		}
+
+	}
+
+}
