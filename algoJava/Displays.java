dfe1df334027a9f4fb23718daf88687c9a503ff9
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-25 10:33:19 -0500

    one example code needs to fix

---
+++ algoJava/Displays.java
@@ -0,0 +1,31 @@
+package clock;
+
+public class Displays {
+	private int value = 0;
+	private int limit = 0;
+	
+	public Displays(int limit) {
+		this.limit = limit;
+	}
+	
+	public void increase() {
+		value++;
+		if (value == limit ) {
+			value = 0;
+		}
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Displays d = new Displays(24);
+		for ( ;; ) {
+			d.increase();
+			System.out.println(d.getValue());
+	}
+
+	}
+}
